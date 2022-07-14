003a21d0fd44adc44459552464e4f17f19e92802
CK286 <CKing682b@gmail.com>
2022-07-14 19:31:32 -0400

    validation

--- scriptHelper.js
+++ scriptHelper.js
@@ -22,8 +22,15 @@ function validateInput(testInput) {
         let form = document.querySelector("form");
         form.addEventListener("submit", function(event) {
             /*add if statements to validate check each field if info not valid  use event.preventDefault();*/
-        })
-    })
+            let pilotNameInput = document.querySelector("input[name=pilotName");
+            let copilotNameInput = document.querySelector("input[name=copilotName");
+            let fuelLevelInput = document.querySelector("input[name=fuelLevel");
+            let cargoMassInput = document.querySelector("input[name=cargoMass");
+            if (pilotNameInput.value === " " || copilotNameInput.value === " " || fuelLevelInput.value === " " || cargoMassInput.value ===" "){
+                event.preventDefault();
+            }
+        });
+    });
    
 }
 
