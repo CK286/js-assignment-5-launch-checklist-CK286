d305e8b38b3230329b8913f9abdbdeb4581b0961
CK286 <CKing682b@gmail.com>
2022-07-15 16:51:43 -0400

    captured input verified

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 window.addEventListener("load", function() {
 
    let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+   /*// Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
@@ -11,7 +11,7 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+   })*/
 
 
 
@@ -23,7 +23,9 @@ window.addEventListener("load", function() {
             let copilotNameInput = document.querySelector("input[name=copilotName]");
             let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
             let cargoMassInput = document.querySelector("input[name=cargoMass]");
+            //alert to verify data captured REMOVE
             alert('pilotname is:' + pilotNameInput.value);
+            
         });
    
 });
