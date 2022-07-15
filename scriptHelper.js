7e65d6a8f3fcd2ab948fc606091b600edf192569
CK286 <CKing682b@gmail.com>
2022-07-14 20:38:41 -0400

    syntax fix

--- scriptHelper.js
+++ scriptHelper.js
@@ -22,10 +22,10 @@ function validateInput(testInput) {
         let form = document.querySelector("form");
         form.addEventListener("submit", function(event) {
             /*add if statements to validate check each field if info not valid  use event.preventDefault();*/
-            let pilotNameInput = document.querySelector("input[name=pilotName");
-            let copilotNameInput = document.querySelector("input[name=copilotName");
-            let fuelLevelInput = document.querySelector("input[name=fuelLevel");
-            let cargoMassInput = document.querySelector("input[name=cargoMass");
+            let pilotNameInput = document.querySelector("input[name=pilotName]");
+            let copilotNameInput = document.querySelector("input[name=copilotName]");
+            let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+            let cargoMassInput = document.querySelector("input[name=cargoMass]");
             if (pilotNameInput.value === " " || copilotNameInput.value === " " || fuelLevelInput.value === " " || cargoMassInput.value ===" "){
                 event.preventDefault();
                 alert("all fields must be filled in");
